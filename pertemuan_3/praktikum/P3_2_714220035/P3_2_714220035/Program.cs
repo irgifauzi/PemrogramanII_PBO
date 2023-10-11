762fe1724c5ae754e1d6ad97f381e3d6cc81edad
Irgi Fauzi <114649826+irgifauzi@users.noreply.github.com>
2023-10-11 16:41:19 +0700

    valo

---
+++ pertemuan_3/praktikum/P3_2_714220035/P3_2_714220035/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3_2_714220035
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Clear();
+            Console.Write("MENENTUKAN ANAK AYAM\n- - - - - - - - - - - - - - - - - - -");
+            Console.Write("\nMasukan Imputan Anak Ayam: ");
+
+            int ayam = Convert.ToInt32(Console.ReadLine());
+            if (ayam >= 1 && ayam <= 10)
+            {
+                for (int i = ayam; i >= 1; i--)
+                {
+                    Console.WriteLine($"Anak ayam turunlah {i}, mati satu tinggalah {i - 1}");
+                }
+                Console.WriteLine("Mati satu tinggalah induknya");
+            }
+            else
+            {
+                Console.WriteLine("Nilai tidak boleh dibawah 1 atau diatas 10");
+            }
+
+        }
+    }
+}
---
+++ pertemuan_3/praktikum/p3_3_714220035/p3_3_714220035/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p3_3_714220035
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             do
+ {
+     Console.WriteLine("PERSEGI PANJANG");
+     Console.WriteLine("1.Luas");
+     Console.WriteLine("2.Keliling");
+
+     Console.Write("Menu Pilihan: ");
+     int input_menu = Convert.ToInt16(Console.ReadLine());
+
+     if (input_menu == 1)
+     {
+         Console.WriteLine("MENGHITUNG PERSEGI PANJANG");
+         Console.Write("Panjang Persegi Panjang: ");
+         int panjang = Convert.ToInt32(Console.ReadLine());
+         Console.Write("Lebar Persegi Panjang: ");
+         int lebar = Convert.ToInt32(Console.ReadLine());
+
+         int luas = panjang * lebar;
+         Console.WriteLine("Luas Persegi Panjang = {0}", luas);
+     }
+
+     else if (input_menu == 2)
+     {
+         Console.WriteLine("MENGHITUNG KELILING PERSEGI PANJANG");
+         Console.Write("Masukkan Panjang: ");
+         int panjang = Convert.ToInt32(Console.ReadLine());
+         Console.Write("Masukkan Lebar: ");
+         int lebar = Convert.ToInt32(Console.ReadLine());
+
+         int keliling = 2 * (panjang + lebar);
+         Console.WriteLine("Luas Persegi Panjang = {0}", keliling);
+     }
+
+     else
+     {
+         Console.WriteLine("tidak terdeteksi");
+     }
+     Console.Write("\nIngin mengulang kembali? (Y/T)");
+ }
+ while (Console.ReadLine() == "Y");
+        }
+    }
+}
