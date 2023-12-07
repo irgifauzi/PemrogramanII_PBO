1967e7c0f81fcac03cd0ac9982f63f83dff6dc3f
Irgi Fauzi <114649826+irgifauzi@users.noreply.github.com>
2023-12-07 10:56:47 +0700

    t

---
+++ pertemuan_10/Praktikum/P10_714220035/P10_714220035/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P10_714220035
+{
+    public partial class Form1 : Form
+    {
+        Koneksi koneksi =new Koneksi();
+
+        public void Tampil()
+        {
+            //Query DB Get MGS
+            DataMahasiswa.DataSource = koneksi.ShowData("SELECT * FROM t_mahasiswa");
+            DataMahasiswa.Columns[0].HeaderText = "NPM";
+            DataMahasiswa.Columns[0].HeaderText = "Nama";
+            DataMahasiswa.Columns[0].HeaderText = "Angkaran";
+            DataMahasiswa.Columns[0].HeaderText = "Alamat";
+            DataMahasiswa.Columns[0].HeaderText = "Email";
+            DataMahasiswa.Columns[0].HeaderText = "No Hp";
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Memanggil method tampil
+            Tampil();
+
+        }
+    }
+}
